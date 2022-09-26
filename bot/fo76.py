e46b49dd4905b49aa19ff00ca754fa0716997259
markgreene74 <markgreene74@users.noreply.github.com>
2022-09-26 17:24:10 +0100

    implement re.Patterns based messages
    - respond to messages based on different re.Patterns
    - use a separate module (fo76) to handle FO76 messages
    - add requests to the requirements

---
+++ bot/fo76.py
@@ -0,0 +1,70 @@
+from datetime import datetime
+import logging
+import os
+import re
+import requests
+import bot.config as cfg
+
+FILENAME = "fo76"
+DIRNAME = os.path.join("bot", ".tmp_data")
+FILE_RELATIVE_PATH = os.path.join(DIRNAME, FILENAME)
+NC_URL = "https://nukacrypt.com/"
+
+logger = logging.getLogger(__name__)
+logger.setLevel(cfg.log_level)
+logger.addHandler(cfg.ch)
+
+
+def _needs_updating():
+    os.makedirs(DIRNAME, exist_ok=True)
+    if not os.path.isfile(FILE_RELATIVE_PATH):
+        logger.debug(f"{FILE_RELATIVE_PATH} does not exist, triggering an update")
+        return True
+
+    modified = datetime.fromtimestamp(os.path.getmtime(FILE_RELATIVE_PATH))
+    days_old = (datetime.today() - modified).days
+    logger.info(f"FO76 last checked: {modified.date()=}")
+
+    return True if days_old >= 7 else False
+
+
+def _get_update():
+    logger.debug("Fetching data")
+
+    ua = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
+    response = requests.get(NC_URL, headers={"User-Agent": ua})
+    logger.debug(f"{response.status_code=}")
+    nc_content = str(response.content)
+
+    silos = re.findall(r"\>(ALPHA|BRAVO|CHARLIE)\<", nc_content)
+    codes = re.findall(r"\>(\d+)\<", nc_content)
+
+    # >>> silos
+    # ['ALPHA', 'BRAVO', 'CHARLIE']
+    # >>> codes
+    # ['61436701', '36758567', '79473176']
+    # >>> list(zip(silos, codes))
+    # [('ALPHA', '61436701'), ('BRAVO', '36758567'), ('CHARLIE', '79473176')]
+    logger.debug(f"{silos=} {codes=}")
+
+    s_c_list = [f"{s}:{c}" for s, c in list(zip(silos, codes))]
+    file_content = "\n".join(s_c_list)
+    logger.debug(f"{file_content=}")
+
+    with open(FILE_RELATIVE_PATH, "w") as f:
+        f.write(file_content)
+    logger.info(f"Updated {FILE_RELATIVE_PATH}")
+
+    return file_content
+
+
+def main():
+    logger.debug("Checking if an update is needed")
+    if _needs_updating():
+        logger.debug("Updating ...")
+        return _get_update()
+    else:
+        logger.debug("An update is NOT needed")
+        with open(FILE_RELATIVE_PATH) as f:
+            data = f.read()
+        return data
--- bot/slack.py
+++ bot/slack.py
@@ -1,6 +1,12 @@
 import logging
+import re
 import bot.config as cfg
+import bot.fo76 as fo76
 from slack_bolt import App
+
+# message patterns
+pattern_fo76 = re.compile(r"[fF][oO]76|\!76|^76$")
+pattern_hello = re.compile(r"[hH](i|ello|ey)")
 
 logger = logging.getLogger(__name__)
 logger.setLevel(cfg.log_level)
@@ -10,9 +16,14 @@ app = App(token=cfg.SLK_BOT_TOKEN)
 logger.debug("Bolt started")
 
 
-# reply to messages containing "hello"
-@app.message("hello")
+# reply to messages containing different patterns
+@app.message(pattern_hello)
 def message_hello(message, say):
-    # say() sends a message to the channel where the event was triggered
-    logger.info(f"hello message - user {message['user']}")
+    logger.info(f"hello requested by user: {message['user']}")
     say(f"Hey there <@{message['user']}>!")
+
+
+@app.message(pattern_fo76)
+def message_fo76(message, say):
+    logger.info(f"FO76 requested by user: {message['user']}")
+    say(f"FO76 silo codes:\n{fo76.main()}")
