621c2bec6fff24fb3dde4902eb9ee70b2dff1ace
danielalazzara <97309237+danielalazzara@users.noreply.github.com>
2023-05-13 15:12:29 +0000

    make messages.py modular
    ... so that different messages file can be used
    
    Co-authored-by: danielalazzara <97309237+danielalazzara@users.noreply.github.com>
    Co-authored-by: markgreene74 <markgreene74@users.noreply.github.com>

--- bot/messages.py
+++ bot/messages.py
@@ -1,23 +1,25 @@
 import json
 import re
+from typing import Pattern
 
 
-def load_data() -> dict:
-    with open("bot/data/messages.json") as f:
+def load_data(file_name: str) -> dict:
+    file_full_path = f"bot/data/{file_name}.json"
+    with open(file_full_path) as f:
         data = f.read()
     return json.loads(data)
 
 
-def find_reply(message: str) -> str:
-    data = load_data()
+def find_reply(message: str, file_name: str) -> str:
+    data = load_data(file_name)
     for key, response in data.items():
         if message.lower() in key:
             return response
     return "I didn't catch that!"
 
 
-def messages_regex() -> str:
-    data = load_data()
+def regex_from_file(file_name: str) -> Pattern[str]:
+    data = load_data(file_name)
     all_keys = []
     for key in data.keys():
         all_keys.append(key)
