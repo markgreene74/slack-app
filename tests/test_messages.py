c7d82197312405fc110cebdafb3a89df8d0845c8
markgreene74 <markgreene74@users.noreply.github.com>
2023-05-20 22:14:06 +0000

    fix test_load_data

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -4,20 +4,28 @@ import pytest
 
 from bot.messages import find_reply, load_data, regex_from_file
 
+LOAD_DATA_EXPECTED = {
+    "firsttest|[fF][iI][rR][sS][tT][ ]*[tT][eE][sS][tT]|testfirst|[tT][eE][sS][tT][fF][iI][rR][sS][tT]": "This is the reply to 'first test' and 'testfirst'",
+    "secondtest|[sS][eE][cC][oO][nN][dD][ ]*[tT][eE][sS][tT]": "This is the reply to 'second test'",
+    "thirdtest|[tT][hH][iI][rR][dD][ ]*[tT][eE][sS][tT]": "This is the reply to 'third test'",
+}
+
 
 @pytest.fixture(autouse=True)
 def change_test_dir(monkeypatch, tmp_path):
+    # create bot/data in the tmp_path
     d = tmp_path / "bot" / "data"
     d.mkdir(parents=True)
+    # and copy the test files in it
     f = d / "test_messages.json"
     shutil.copy("tests/test_data/test_messages.json", f)
-    monkeypatch.chdir(d)
+
+    monkeypatch.chdir(tmp_path)
 
 
-def test_load_data(monkeypatch):
-    # test_data = load_data("test_messages")
-    # assert test_data == ""
-    pass
+def test_load_data():
+    test_data = load_data("test_messages")
+    assert test_data == LOAD_DATA_EXPECTED
 
 
 def test_find_reply():
